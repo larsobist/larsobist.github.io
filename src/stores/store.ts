889bb61d1a42f4450c7edc2df29bd338376dd4f6
Lars Obist <s91242@bht-berlin.de>
2023-11-19 16:29:34 +0100

    implemented i18n

--- src/stores/store.ts
+++
@@ -1,7 +0,0 @@
-import { ref } from 'vue';
-
-export const selectedProject = ref<string | null>(null);
-
-export function setSelectedProject(project: string | null) {
-  selectedProject.value = project;
-}
