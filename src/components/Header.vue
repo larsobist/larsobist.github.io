4bf3a46cfc7c41d6a61de4e0bfb791bec22eca6b
Lars Obist <s91242@bht-berlin.de>
2024-01-15 16:18:16 +0100

    header done

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,68 @@
 <script lang="ts" setup>
 
-import {apppearance} from "@/stores/store";
+import CONTENT from "@/locales/content.json";
 </script>
 
 <template>
 
-    <h2>{{ $t('HEADER.TITLE') }}</h2>
-  hello! i'm a software developer & designer crafting meaningful identities and experiences.
-
+  <div class="header-container">
+    <div class="header-content">
+      <h2>{{ $t('LARSOBIST') }}</h2>
+      {{ $t('HEADER.TEXT') }}
+      <p></p>
+    </div>
+    <img :src="CONTENT.BRACKETS" alt="{}" class="img-header img-brackets"/>
+    <img :src="CONTENT.HASHTAG" alt="#" class="img-header img-hashtag"/>
+    <img :src="CONTENT.SYNTAX" alt="</>" class="img-header img-syntax"/>
+  </div>
 
 </template>
 
 <style scoped>
+
+.header-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+}
+
+.header-content {
+  text-align: center;
+  padding: 5px;
+}
+
+.img-header {
+  position: absolute;
+  height: 500px;
+}
+
+.img-brackets {
+  transform: translateX(20%) translateY(-30%);
+}
+
+.img-hashtag {
+  transform: translateX(-40%) translateY(20%);
+}
+
+.img-syntax {
+  transform: translateX(30%) translateY(40%);
+}
+
+@media screen and (max-width: 600px) {
+  .img-brackets {
+    transform: translateX(10%) translateY(-40%);
+  }
+
+  .img-hashtag {
+    transform: translateX(-30%) translateY(25%);
+  }
+
+  .img-syntax {
+    transform: translateX(20%) translateY(45%);
+  }
+}
 
 </style>
