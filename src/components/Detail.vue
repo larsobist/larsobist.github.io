b8dd6bbdd6b409baa841e7c7729423c8eb015c56
Lars Obist <s91242@bht-berlin.de>
2024-01-23 20:50:46 +0100

    tools detail

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -34,13 +34,13 @@ export type ProjectKey =
   <div v-if="selectedProject" class="displayProject">
     <h2>{{ $t('DETAIL.TITLE') }}</h2>
     <div class="grid">
-      <div :class="[getAppearanceClass(), 'project-header']">
-        <div >
+      <div :class="[getAppearanceClass(), 'full-width']">
+        <div>
           <h3>{{ CONTENT.PROJECTS[selectedProject as ProjectKey].TITLE }}</h3>
           {{ $t('PROJECTS.' + selectedProject + '.CATEGORY') }}
         </div>
         <div class="close-button" @click="clearSelection">
-            {{ $t('DETAIL.CLOSE') }}
+          {{ $t('DETAIL.CLOSE') }}
           <img :src="appearance === 'Dark' ? CONTENT.CLOSELIGHT : CONTENT.CLOSEDARK" alt="Lightode" class="close-img"/>
         </div>
       </div>
@@ -49,8 +49,8 @@ export type ProjectKey =
     <div class="grid">
       <div :class="[getAppearanceClass(), 'smaller']">
         <div class="text">
-        {{ $t('PROJECTS.' + selectedProject + '.DESCRIPTION') }}<br>
-      </div>
+          {{ $t('PROJECTS.' + selectedProject + '.DESCRIPTION') }}<br>
+        </div>
       </div>
       <div :class="[getAppearanceClass(), 'bigger']">
         <carousel id="gallery" :autoplay="5000" :items-to-show="1" :wrap-around="true">
@@ -65,31 +65,37 @@ export type ProjectKey =
         </carousel>
       </div>
     </div>
-    <div class="grid">
-      <div :class="[getAppearanceClass(), 'smaller', 'main-left']">
-        <div class="responsive-row">
+
+    <div class="full-width">
+        <div class="used-tools">
+
           <img v-for="(image, index) in CONTENT.PROJECTS[selectedProject as ProjectKey].TECH" :key="index" :src="image"
-               alt="Logo" class="img"/>
+               alt="Logo" :class="[getAppearanceClass(), 'tools-img']"/>
         </div>
+
+      <div class="buttons">
+      <div v-if="CONTENT.PROJECTS[selectedProject as ProjectKey].VIDEOLINK!=''" :class="[getAppearanceClass()]">
+        <a :href="CONTENT.PROJECTS[selectedProject as ProjectKey].VIDEOLINK" target="_blank">{{
+            $t('DETAIL.VIDEO')
+          }}</a>
       </div>
-      <div :class="[getAppearanceClass(), 'bigger', 'main-right']">
-        {{ CONTENT.PROJECTS[selectedProject as ProjectKey].VIDEOLINK }}<br>
-        <div v-if="CONTENT.PROJECTS[selectedProject as ProjectKey].DOCULINK!=''" class="btn">
-          <a :href="CONTENT.PROJECTS[selectedProject as ProjectKey].DOCULINK" target="_blank">{{
-              $t('DETAIL.DOCU')
-            }}</a>
-        </div>
-        <div v-if="CONTENT.PROJECTS[selectedProject as ProjectKey].GITHUBLINK!=''" class="btn">
-          <a :href="CONTENT.PROJECTS[selectedProject as ProjectKey].GITHUBLINK" target="_blank">{{
-              $t('DETAIL.GITHUB')
-            }}</a>
-        </div>
-        <div v-if="CONTENT.PROJECTS[selectedProject as ProjectKey].LIVELINK!=''" class="btn">
-          <a :href="CONTENT.PROJECTS[selectedProject as ProjectKey].LIVELINK" target="_blank">{{
-              $t('DETAIL.LIVEDEMO')
-            }}</a>
-        </div>
+      <div v-if="CONTENT.PROJECTS[selectedProject as ProjectKey].DOCULINK!=''" :class="[getAppearanceClass()]">
+        <a :href="CONTENT.PROJECTS[selectedProject as ProjectKey].DOCULINK" target="_blank">{{
+            $t('DETAIL.DOCU')
+          }}</a>
       </div>
+      <div v-if="CONTENT.PROJECTS[selectedProject as ProjectKey].GITHUBLINK!=''" :class="[getAppearanceClass()]">
+        <a :href="CONTENT.PROJECTS[selectedProject as ProjectKey].GITHUBLINK" target="_blank">{{
+            $t('DETAIL.GITHUB')
+          }}</a>
+      </div>
+      <div v-if="CONTENT.PROJECTS[selectedProject as ProjectKey].LIVELINK!=''" :class="[getAppearanceClass()]">
+        <a :href="CONTENT.PROJECTS[selectedProject as ProjectKey].LIVELINK" target="_blank">{{
+            $t('DETAIL.LIVEDEMO')
+          }}</a>
+      </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -99,7 +105,6 @@ export type ProjectKey =
   display: grid;
   grid-template-columns: repeat(7, 1fr);
   grid-template-rows: repeat(1, 1fr);
-  gap: 0;
 }
 
 .smaller {
@@ -110,10 +115,11 @@ export type ProjectKey =
   grid-column: span 4;
 }
 
-.project-header {
+.full-width {
   grid-column: span 7;
   display: flex;
   justify-content: space-between;
+  padding: 5px;
 }
 
 .close-button {
@@ -135,9 +141,14 @@ export type ProjectKey =
   width: 100%;
 }
 
-.responsive-row {
+.used-tools {
   display: flex;
   flex-direction: row;
+}
+
+.tools-img{
+  margin-right: 10px;
+  height: 75px;
 }
 
 @media screen and (max-width: 800px) {
@@ -150,7 +161,7 @@ export type ProjectKey =
     grid-template-columns: repeat(4, 1fr);
   }
 
-  .smaller, .bigger{
+  .smaller, .bigger {
     grid-column: span 2;
   }
 }
@@ -158,10 +169,6 @@ export type ProjectKey =
 @media screen and (max-width: 800px) {
   .grid {
     grid-template-columns: repeat(1, 1fr);
-  }
-
-  .main-left, .main-right {
-    grid-column: span 1;
   }
 }
 </style>
